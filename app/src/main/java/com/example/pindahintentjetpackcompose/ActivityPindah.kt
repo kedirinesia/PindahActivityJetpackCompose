1df4ce9c4cef345ac83cec29bdc5fa5d9a412b4b
kedirinesia <pulungwicaksono2006@gmail.com>
2024-08-29 11:00:43 +0700

    Pindah Activity Kotlin Jetpack Studio

---
+++ app/src/main/java/com/example/pindahintentjetpackcompose/ActivityPindah.kt
@@ -0,0 +1,16 @@
+package com.example.pindahintentjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityPindah : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pindah)
+
+    }
+}
---
+++ app/src/main/java/com/example/pindahintentjetpackcompose/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.pindahintentjetpackcompose
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pindahintentjetpackcompose.ui.theme.PindahIntentJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize()
+                    .wrapContentSize(Alignment.Center)
+            ) {
+                Button(onClick = {
+                    startActivity(Intent(applicationContext, ActivityPindah::class.java))
+                }) {
+                    Text(text = "Pindah Activity")
+                }
+            }
+
+            }
+        }
+
+    }
